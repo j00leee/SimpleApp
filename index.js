fe0d06d51dda639a33dd8e4f6043fd4da334a4b1
j00leee <julie.minjung.chung@gmail.com>
2021-09-19 16:08:22 -0400

    changes

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import userRouter from "./routes/user.js";
+import userRouter from "./routes/users.js";
 
 const app = express();
 
@@ -70,13 +70,14 @@ app.use(cors());
 
 app.use("/user", userRouter);
 
-const CONNECTION_URL = process.env.MONGO_URI;
-const PORT = process.env.PORT|| 5000;
+//const CONNECTION_URL = process.env.MONGO_URI;
+//const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+mongoose.connect(process.env.MONGO_URI, 
+  {useNewUrlParser: true, useUnifiedTopology: true}
+  ); 
 
-mongoose.set('useFindAndModify', false);
+
+//mongoose.set('useFindAndModify', false);
 
   
