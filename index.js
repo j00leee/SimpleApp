7d68e921c605312c41b354ec099d9cd68891654a
j00leee <julie.minjung.chung@gmail.com>
2021-09-20 04:14:46 -0400

    server and client running at the same time

--- index.js
+++ index.js
@@ -1,83 +1,58 @@
-/*import express from 'express';
-import mongoose from 'mongoose';
-
-
-const express = require('express')
-require('dotenv').config();
-const app = express();
-const port = process.env.PORT
-
-
-import userRouter from './routes/users.js';
-
- app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/chance', (req, res) => {
-    res.send('chance is cool')
-})
-
-app.get('/signin', (req, res) => {
-  res.send('chance is cool')
-}) 
-
-app.use('/user', userRouter);
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-var mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI, 
-{useNewUrlParser: true, useUnifiedTopology: true}
-);
-
-var nameSchema = new mongoose.Schema({
-    userName: String,
-    password: String
-   });
-
-   var User = mongoose.model("User", nameSchema);
-
-const newUser = new User({userName: 'julie', password: 'chanceiscute'}).save();
-
-if (process.env.NODE_ENV === 'production') {
-    const path = require('path');
-    console.log(path.join(__dirname, 'simpleapp', 'build', 'index.html'));
-    app.use(express.static(__dirname));
-    app.use(express.static(path.join(__dirname, 'simpleapp', 'build')));
-    app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'simpleapp', 'build', 'index.html'));
-      console.log(path.join(__dirname, 'simpleapp', 'build', 'index.html'));
-    });
-  } */
-
-  
+//new version of node
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+//const express = require('express');
+
+import postRoutes from './routes/posts.js';
 import userRouter from "./routes/users.js";
 
+
+
 const app = express();
+dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.get('/', (req, res) => {
+  res.send('Hello to pcdemoapp');
+});
+
+app.use(bodyParser.json({ limit: "30mb", extended: true  }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true  }));
 app.use(cors());
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // to enable calls from every domain 
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE'); // allowed actiosn
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+  
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200); // to deal with chrome sending an extra options request
+    }
+  
+    next(); // call next middlewer in line
+  });
+app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-//const CONNECTION_URL = process.env.MONGO_URI;
-//const PORT = process.env.PORT|| 5000;
+//const CONNECTION_URL = 'mongodb+srv://pcdemouser:pcdemouser123@cluster0.suy2y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, 
-  {useNewUrlParser: true, useUnifiedTopology: true}
-  ); 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
+    if (process.env.NODE_ENV === 'production') {
+      const path = require('path');
+      console.log(path.join(__dirname, 'simpleapp', 'build', 'index.html'));
+      app.use(express.static(__dirname));
+      app.use(express.static(path.join(__dirname, 'simpleapp', 'build')));
+      app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'simpleapp', 'build', 'index.html'));
+        console.log(path.join(__dirname, 'simpleapp', 'build', 'index.html'));
+      });
+    }
 
+    console.log(process.env.Test);
 //mongoose.set('useFindAndModify', false);
-
-  
