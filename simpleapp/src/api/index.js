b247e61b6059947495cd0ddf4cae7da35bc9ed87
j00leee <julie.minjung.chung@gmail.com>
2021-09-21 16:09:12 -0400

    changes

--- simpleapp/src/api/index.js
+++ simpleapp/src/api/index.js
@@ -3,7 +3,25 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 const API2 = axios.create({ baseURL: 'https://firstdeployproject.herokuapp.com/' });
 
-API.interceptors.request.use((req) => {
+if(baseURL === 'http://localhost:5000'){
+  API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+  
+    return req;
+  });
+}
+else{
+  API2.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+  
+    return req;
+  });
+}
+/*API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
   }
@@ -23,7 +41,7 @@ API2.interceptors.request.use((req) => {
 //export const createPost = (newPost) => API.post('/posts', newPost);
 //export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 //export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
-//export const deletePost = (id) => API.delete(`/posts/${id}`);
+//export const deletePost = (id) => API.delete(`/posts/${id}`); */
 
 export const signIn = (formData) => API.post('/user/signin', formData);
 export const signUp = (formData) => API.post('/user/signup', formData);
