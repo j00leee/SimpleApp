982c4d6adf31dce95b7838a2adf05ac7c4f626bc
j00leee <julie.minjung.chung@gmail.com>
2021-09-21 16:13:26 -0400

    changes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ import express from "express";
 const router = express.Router();
 
 import { signin, signup } from "../controllers/user.js";
+import { signin2, signup2 } from "../controllers/user.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
+
+router.post("/signin2", signin2);
+router.post("/signup2", signup2);
 
 export default router;
--- simpleapp/src/api/index.js
+++ simpleapp/src/api/index.js
@@ -3,25 +3,7 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 const API2 = axios.create({ baseURL: 'https://firstdeployproject.herokuapp.com/' });
 
-if(baseURL === 'http://localhost:5000'){
-  API.interceptors.request.use((req) => {
-    if (localStorage.getItem('profile')) {
-      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
-    }
-  
-    return req;
-  });
-}
-else{
-  API2.interceptors.request.use((req) => {
-    if (localStorage.getItem('profile')) {
-      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
-    }
-  
-    return req;
-  });
-}
-/*API.interceptors.request.use((req) => {
+API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
   }
@@ -41,7 +23,7 @@ API2.interceptors.request.use((req) => {
 //export const createPost = (newPost) => API.post('/posts', newPost);
 //export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 //export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
-//export const deletePost = (id) => API.delete(`/posts/${id}`); */
+//export const deletePost = (id) => API.delete(`/posts/${id}`);
 
 export const signIn = (formData) => API.post('/user/signin', formData);
 export const signUp = (formData) => API.post('/user/signup', formData);
