9cc45ccd8d870ce21e6b4ef68d1920078e72b6ce
j00leee <julie.minjung.chung@gmail.com>
2021-09-23 01:45:48 -0400

    reading and displaying data and autocomplete search bar

--- simpleapp/src/components/search.js
+++ simpleapp/src/components/search.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+//import Autocomplete from "react-google-autocomplete";
+import { stockData } from "./Data/data";
+
 
 
 const Search = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+     
+
     return (
-        <div>
+        <>
+  
+      <div className="stock-container">
+        {stockData.filter((data)=> {
+            if(searchTerm == "") {
+                return data
+            } else if (data.company.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return data
+            }
+        }).map((data, key) => {
+          return (
+            <div key={key}>
+              {data.company +
+                " , " +
+                data.ticker +
+                " ," +
+                data.stockPrice +
+                ", " +
+                data.timeElapsed}
+            </div>
+          );
+        })}
+      </div>
+      <div>
+          <input
+           type="text"
+           placeholder="search..."
+           onChange={event => {
+               setSearchTerm(event.target.value);
+            }}
+          />
+      </div>
+    </>
+    /* <div>
             hello
-        </div>
+            {
+                data && data.length>0 && data.map((item)=><p>{item.address}</p>)
+            }
+            <Autocomplete
+                apiKey={'1220afd0012e7145ef6018001b3ccf43'}
+                onPlaceSelected={(place) => {
+                    console.log(place);
+                }}
+            /> 
+            </div> */
     )
 }
 
